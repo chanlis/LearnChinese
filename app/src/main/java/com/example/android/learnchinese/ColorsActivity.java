8d605b933ad337e0d6d3a2c39f1e8185f15aa2a4
Lisa Chan <chan.lis@husky.neu.edu>
2017-05-13 17:11:34 -0400

    code cleanup :)

--- app/src/main/java/com/example/android/learnchinese/ColorsActivity.java
+++ app/src/main/java/com/example/android/learnchinese/ColorsActivity.java
@@ -14,7 +14,7 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.words_list);
 
         // list of words for colors
-        ArrayList<Word> colors = new ArrayList<Word>();
+        final ArrayList<Word> colors = new ArrayList<Word>();
         initColors(colors);
 
         // init list view
@@ -24,31 +24,31 @@ public class ColorsActivity extends AppCompatActivity {
 
     }
 
-    private final void initColors(ArrayList<Word> colors)
+    private void initColors(ArrayList<Word> colors)
     {
         String packageName = this.getPackageName();
-        colors.add(new Word(getResources().getString(R.string.white_color), "白色", "bái sè",
-                getResources().getIdentifier("white", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.black_color), "黑色", "hēi sè",
-                getResources().getIdentifier("black", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.red_color), "红色", "hóng sè",
-                getResources().getIdentifier("red", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.yellow_color), "黄色", "huáng sè",
-                getResources().getIdentifier("yellow", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.green_color), "绿色", "lǜ sè",
-                getResources().getIdentifier("green", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.blue_color), "蓝色", "lán sè",
-                getResources().getIdentifier("blue", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.brown_color), "褐色", "hé sè",
-                getResources().getIdentifier("brown", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.orange_color), "橙色", "chéng sè",
-                getResources().getIdentifier("orange", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.grey_color), "灰色", "huī sè",
-                getResources().getIdentifier("grey", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.pink_color), "粉红色色", "fěn hóng sè",
-                getResources().getIdentifier("pink", "raw", packageName)));
-        colors.add(new Word(getResources().getString(R.string.purple_color), "紫色", "zǐ sè",
-                getResources().getIdentifier("purple", "raw", packageName)));
+        colors.add(new Word(getString(R.string.white_color), "白色", "bái sè",
+                R.raw.white));
+        colors.add(new Word(getString(R.string.black_color), "黑色", "hēi sè",
+                R.raw.black)));
+        colors.add(new Word(getString(R.string.red_color), "红色", "hóng sè",
+                R.raw.red)));
+        colors.add(new Word(getString(R.string.yellow_color), "黄色", "huáng sè",
+                R.raw.yellow)));
+        colors.add(new Word(getString(R.string.green_color), "绿色", "lǜ sè",
+                R.raw.green)));
+        colors.add(new Word(getString(R.string.blue_color), "蓝色", "lán sè",
+                R.raw.blue)));
+        colors.add(new Word(getString(R.string.brown_color), "褐色", "hé sè",
+                R.raw.brown)));
+        colors.add(new Word(getString(R.string.orange_color), "橙色", "chéng sè",
+                R.raw.orange)));
+        colors.add(new Word(getString(R.string.grey_color), "灰色", "huī sè",
+                R.raw.grey)));
+        colors.add(new Word(getString(R.string.pink_color), "粉红色色", "fěn hóng sè",
+                R.raw.pink)));
+        colors.add(new Word(getString(R.string.purple_color), "紫色", "zǐ sè",
+                R.raw.purple)));
     }
 
 }
